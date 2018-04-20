9d8f713903b0a88b913835e536c8cf9a9a612e34
Adi Vaknin <deepspace2@users.noreply.github.com>
2018-04-20 21:19:35 +0300

    Added solution to Kattis/Tarif problem

---
+++ Kattis/Tarifa/tarifa.py
@@ -0,0 +1,5 @@
+from functools import reduce
+
+mb_per_month, num_of_months = (int(input()) for _ in range(2))
+usages = (int(input()) for _ in range(num_of_months))
+print(reduce(lambda x, y: x + mb_per_month - y, usages, mb_per_month))
