aeeb2c1fcab59df4cf2190283f895d3b4782ab1f
Adi Vaknin <deepspace2@users.noreply.github.com>
2018-05-16 22:32:43 +0300

    Added solution to exercism/Binary problem

---
+++ Exercism.io/Binary/binary.cr
@@ -0,0 +1,12 @@
+module Binary
+    extend self
+  
+    def to_decimal(binary : String) : Int
+      raise Exception.new if binary.chars.any? { |e| !['0', '1'].includes?(e) }
+      dec = 0
+      binary.chars.reverse.each_with_index do |digit, index|
+        dec += digit.to_i * (2 ** index) if digit == '1'
+      end
+      dec
+    end
+  end
