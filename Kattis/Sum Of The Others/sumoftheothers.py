85c5724647aac8220bc97f5292566e7f1c7caf5e
Adi Vaknin <deepspace2@users.noreply.github.com>
2018-06-16 15:30:03 +0300

    Added solution to Kattis/Sum of the Others problem

---
+++ Kattis/Sum Of The Others/sumoftheothers.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        line = input().strip()
+    except EOFError:
+        break
+    line = list(map(int, line.split()))
+    for num in line:
+        num_index = line.index(num)
+        excluded_line = line[:num_index] + line[num_index + 1:]
+        if sum(excluded_line) == num:
+            print(num)
+            break
